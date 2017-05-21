70413655b87176fb966a80eb7c5373c25cbf7185
高松幸平 <gaoxin.xixxix@gmail.com>
2017-05-21 21:52:49 +0900

    Initial Commit

---
+++ playWithMoya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  playWithMoya
+//
+//  Created by 高松幸平 on 2017/05/21.
+//  Copyright © 2017年 gaopin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
