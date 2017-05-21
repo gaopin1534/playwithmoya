adbb8baaba0d10e868a0588bbd7712244f3b70c8
高松幸平 <gaoxin.xixxix@gmail.com>
2017-05-21 23:15:09 +0900

    apply target type

---
+++ playWithMoya/HackersNewsAPI.swift
@@ -0,0 +1,72 @@
+//
+//  HackersNewsAPI.swift
+//  playWithMoya
+//
+//  Created by 高松幸平 on 2017/05/21.
+//  Copyright © 2017年 gaopin. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+enum HackersNewsAPI {
+    case item(id: String)
+    case user(id: String)
+    case maxItem
+    case topStories
+    case newStories
+}
+
+extension HackersNewsAPI: TargetType {
+    // ベースURL
+    var baseURL: URL {
+        return URL(string: "https://hacker-news.firebaseio.com/v0")!
+    }
+    
+    // それぞれのターゲットごとのpath
+    var path: String {
+        switch self {
+        case .item(let id), .user(let id):
+            return "/\(id).json"
+        case .maxItem:
+            return "/maxitem.json"
+        case .newStories:
+            return "/newstories"
+        case .topStories:
+            return "/topstories"
+        }
+    }
+    
+    // それぞれのターゲットごとのhttpメソッド
+    // 今回はgetしかないが、postなどある場合はswitch selfなどで適切な値を返す
+    var method: Moya.Method {
+        switch  self {
+        case .item, .user, .maxItem, .newStories, .topStories:
+            return .get
+        }
+    }
+
+    // 今回はパラメーターとして渡さないのですべてnil
+    var parameters: [String: Any]? {
+        return nil
+    }
+    
+    // パラメーターのエンコーディング指定
+    // リクエストボデイにjsonとしてセットすることもできる
+    var parameterEncoding: ParameterEncoding {
+        return URLEncoding.default
+    }
+    
+    // テスト時に使われる(なんでマストなのかわからない)
+    // めんどくさかったので今回はずるします
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch  self {
+        case .item, .user, .maxItem, .newStories, .topStories:
+            return .request
+        }
+    }
+}
